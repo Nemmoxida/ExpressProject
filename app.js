bbb1c4176aa0749141cb82fc8e5bad48d87339ec
nemmoxida <ilham.kume02@gmail.com>
2025-03-28 13:38:09 +0700

    add logging feature

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ import errorHandling from "./Middleware/errorHandling.js";
 import { v4 as uuidv4 } from "uuid";
 import mysql from "./Database/Connection.js";
 import userRepository from "./Repository/userRepository.js";
+import logger from "./log/logger.js";
+import morgan from "morgan";
 
 dotenv.config();
 const app = express();
 const uuid = uuidv4();
 const userRepo = new userRepository(mysql);
+
+// Middleware for logging requests
+app.use(morgan("dev"));
 
 // Middleware for parsing JSON
 app.use(express.json());
@@ -23,6 +28,6 @@ route(app, express, jwt, uuid, userRepo);
 app.use(errorHandling);
 
 // Initiate server
-server(app);
+server(app, logger);
 
 export default app;
--- server.js
+++ server.js
@@ -2,8 +2,9 @@
  * @param {import('express').Express} app
  */
 
-export default function sever(app) {
+export default function sever(app, logger) {
   app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    // console.log("Server is running on port 3000");
+    logger.info("Server is running on port 3000");
   });
 }
