c418412d53f518e8dda6580ebffcdec8fa99c60e
Ilham Fauzy <ilham.kume02@gmail.com>
2026-01-21 20:33:33 +0700

    some comment for help

--- Controllers/pokemon/getPokemon.js
+++ Controllers/pokemon/getPokemon.js
@@ -3,15 +3,20 @@ import respondHanlder from "../../Middleware/respondHandler.js";
 
 export default function getPokemon() {
   return async (req, res, next) => {
+    // extrack the pokemon that client will request
     const pokemon = req.query.pokemon;
 
+    // validation
     if (!pokemon) {
+      // err handling
       return next(new AppError("Pokemon name not detected", 400));
     }
 
+    // request to external API
     const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await request.json();
 
+    // for respond
     const respond = respondHanlder(data, 200, "Data success fully retrieved");
 
     return res.status(200).json(respond);
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,7 +3,11 @@ import getPokemon from "../Controllers/pokemon/getPokemon.js";
 export default function pokemon(express) {
   const router = express.Router();
 
+  // localhost:3000/pokemon
   router.get("/", getPokemon());
+
+  // localhost:3000/pokemon/abbilities
+  // router.get("/abilities");
 
   return router;
 }
